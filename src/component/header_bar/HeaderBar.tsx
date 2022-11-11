c872afc6e300d68d312682fe73666955372d3ba8
Pociatlo <k.podsadlo@gmail.com>
2022-11-11 12:17:50 +0100

    names change

--- src/component/header_bar/HeaderBar.tsx
+++ src/component/header_bar/HeaderBar.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import styles from "./header_tile.module.scss";
-import { constants } from 'fs/promises';
+import styles from "./HeaderBar.module.scss";
+
 interface PropsTile{
     text:string,
 }
+
 const Tile : React.FC <PropsTile> = ({text}) =>{
     return(
         <div className={styles.tile}> {text} </div>
 
     );
-
 }
 export default Tile;
