9645b1ec8b95967d0381c632aa3e227f2524663d
TacticalCrab <pkaleksandrowicz@gmail.com>
2022-11-16 10:20:51 +0100

    fix structure

--- src/api/team/Team.service.tsx
+++ src/api/team/Team.service.tsx
@@ -3,13 +3,7 @@ import { axiosInstance } from "../AxiosInstance";
 
 export const getTeamInfoByTeamId = async (teamId: string) => {
     return axiosInstance.get(`teams/${teamId}`);
-}    
-
-
-export const getUsersInfoByTeamId = async (teamId: string) => {
-    return axiosInstance.get(`users?team_id=${teamId}`);
 }
-
 
 export const getInvitesInfoByTeamId = async (teamId: string) => {
     return axiosInstance.get(`invites?team_id=${teamId}`);
