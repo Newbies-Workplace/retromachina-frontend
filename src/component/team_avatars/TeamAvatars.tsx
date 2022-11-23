26c7b89c72d000506f24e695f61635ee26a5e918
XILLLU <milosz.szymon2014@gmail.com>
2022-11-23 16:00:28 +0100

    fix position

---
+++ src/component/team_avatars/TeamAvatars.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { UserResponse } from "../../api/user/User.interfaces";
+import styles from '../../component/team_avatars/TeamAvatars.module.scss'
+import { User } from "../../interfaces/User.interface";
+import {Avatar} from '../avatar/Avatar'
+
+interface TeamAvatarProps{
+//TODO: zmiana typu danych usera
+    users:{
+        isActive:boolean,
+        avatar_link:string
+    }[]
+    
+}
+
+export const TeamAvatar:React.FC<React.PropsWithChildren<TeamAvatarProps>> = ({children,users})=>{
+    const usersLength = users.length;
+    return(
+        <div className={styles.wrapper}>
+            {users.map((user,index)=>{
+                const calcZIndex = usersLength+(index+1)
+                return(
+                    //TODO:user nie ma isActive 
+                    <div className={styles.avatar}>
+                    <Avatar isActive={user.isActive} url={user.avatar_link} style={{zIndex:calcZIndex}}></Avatar>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
