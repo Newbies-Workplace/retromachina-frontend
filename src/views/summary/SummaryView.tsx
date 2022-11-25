cdf982ab3b7598363531cce1fcf7ccb7d98ff3dd
XILLLU <milosz.szymon2014@gmail.com>
2022-11-25 14:16:59 +0100

    summary view wip

--- src/views/summary/SummaryView.tsx
+++ src/views/summary/SummaryView.tsx
@@ -1,41 +1,61 @@
 import React from "react";
 import { Avatar } from "../../component/avatar/Avatar";
+import { Card } from "../../component/card/Card";
 import Navbar from "../../component/navbar/Navbar";
 import styles from '../summary/SummaryView.module.scss'
+import QuestionMarkSvg from '../../assets/icons/question-mark.svg'
 
 
 interface SummaryViewProps{
     users:{
-        value:number,
+        avatar_link:string,
+        isActive:boolean,
+        nick:string,
     }[]
     cards:{
-        value:number,
+        text:string,
     }[]
 }
 
 export const SummaryView: React.FC<React.PropsWithChildren<SummaryViewProps>> = ({users,cards})=>{
     return(
         <>
-            <Navbar/>
+            <Navbar>
+                <div className={styles.summaryField}>
+                    Podsumowanie  
+                    <QuestionMarkSvg style={{marginLeft:"40px"}}></QuestionMarkSvg>
+                </div>
+            </Navbar>
             <div className={styles.container}>
                 <div className={styles.wrapper}>
                     <div className={styles.textSection}>
                         <div className={styles.textFile}>Retro #3 23.12.2022</div>
                     </div>
-                    
-                    <div className={styles.authorAndCardSection}>
-                        <div className={styles.authorSection}>
-                            <Avatar url= {"https://lh3.googleusercontent.com/a/ALm5wu3SC-7-heYYInoj9MSiA9rsQ91W_smQI9bcn9rp=s96-c"
-                                        } isActive={true}>
-                            </Avatar>
-                        </div>
+                    {users.map((user)=>{
+                        return(
+                            <div className={styles.authorAndCardSection}>
+                                <div className={styles.authorSection}>
+                                    <Avatar url={user.avatar_link} isActive={user.isActive}></Avatar>
+                                    {user.nick}
+                                </div>
+                                {cards.map((card)=>{
+                                    return(
+                                        <div className={styles.cardSection}>
+                                            <div className={styles.blackcard}></div>
+                                        </div>
+                                    )
 
-                        <div className={styles.cardSection}>
-                            <div className={styles.blackcard}></div>
-                            <div className={styles.blackcard}></div>
-                            <div className={styles.blackcard}></div>           
-                        </div>                  
-                    </div>
+                                })
+                            }
+                            </div>
+                            
+                            )
+                            
+                            
+                        })
+                    }
+        
+                        
                      
                 </div>
             </div>
