aa9ec1a4b69cbfc3b77cbe73bb0c61de9fb53848
XILLLU <milosz.szymon2014@gmail.com>
2022-11-25 09:32:58 +0100

    summary view

--- src/views/auth/signIn/SignInView.tsx
+++ src/views/auth/signIn/SignInView.tsx
@@ -2,27 +2,28 @@ import React from 'react';
 import GoogleButton from 'react-google-button';
 import styles from './SignInView.module.scss';
 import LogoSvg from '../../../assets/images/logo.svg'
+import { SummaryView } from '../../summary/SummaryView';
 
 
 export const SignInView: React.FC = () => {
     const href = API_URL + "google/redirect";
-
+   
     return (
-        
-        <div className={styles.container}>
+        <SummaryView/>
+        // <div className={styles.container}>
             
             
-             <div className={styles.dialog}>
+        //      <div className={styles.dialog}>
             
-                <div className={styles.text}>
-                    <LogoSvg />
-                    <span>powered by <a href="http://newbies.pl">Newbies</a></span>
-                </div>
+        //         <div className={styles.text}>
+        //             <LogoSvg />
+        //             <span>powered by <a href="http://newbies.pl">Newbies</a></span>
+        //         </div>
 
-                <a href={href}>
-                    <GoogleButton className={styles.googleBtn} />
-                </a>
-            </div> 
-        </div>
+        //         <a href={href}>
+        //             <GoogleButton className={styles.googleBtn} />
+        //         </a>
+        //     </div> 
+        // </div>
     );
 };
---
+++ src/views/summary/SummaryView.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Avatar } from "../../component/avatar/Avatar";
+import { Card } from "../../component/card/Card";
+import { Input } from "../../component/input/Input";
+import Navbar from "../../component/navbar/Navbar";
+import styles from '../summary/SummaryView.module.scss'
+
+
+interface SummaryViewProps{
+    users:{
+        value:number,
+    }[]
+    cards:{
+        value:number,
+    }[]
+}
+
+export const SummaryView: React.FC<React.PropsWithChildren<SummaryViewProps>> = ({users,cards})=>{
+    return(
+        <>
+            <Navbar/>
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <div className={styles.textSection}>
+                        <div className={styles.textFile}>Retro #3 23.12.2022</div>
+                    </div>
+                    
+                    <div className={styles.authorAndCardSection}>
+                        <div className={styles.authorSection}>
+                            <Avatar url= {"https://lh3.googleusercontent.com/a/ALm5wu3SC-7-heYYInoj9MSiA9rsQ91W_smQI9bcn9rp=s96-c"
+                                        } isActive={true}>
+                            </Avatar>
+                        </div>
+
+                        <div className={styles.cardSection}>
+                            <div className={styles.blackcard}></div>
+                            <div className={styles.blackcard}></div>
+                            <div className={styles.blackcard}></div>           
+                        </div>                  
+                    </div>
+                     
+                </div>
+            </div>
+        </>
+    );
+
+}  
