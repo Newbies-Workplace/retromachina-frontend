c1f587ca7215eae3d9626ee175e32a5b8a32d6fd
JUtupe <petryszyn@o2.pl>
2022-12-18 02:28:19 +0100

    better counter indicator

--- src/component/card/counter/Counter.tsx
+++ src/component/card/counter/Counter.tsx
@@ -1,49 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import AddIcon from "../../../assets/icons/add-icon.svg";
 import MinusIcons from "../../../assets/icons/minus-icon.svg";
 import styles from "./Counter.module.scss";
 import cs from "classnames";
 
 interface CounterProps {
-  canIncrement: boolean;
-  count: number;
-  onIncrement(): void;
-  onDecrement(): void;
+    canIncrement: boolean;
+    count: number;
+    onIncrement(): void;
+    onDecrement(): void;
 }
 
-const Counter: React.FC<CounterProps> = ({
-  count,
-  canIncrement,
-  onIncrement,
-  onDecrement,
-}) => {
-  const onMinusClick = () => {
-    if (count > 0) onDecrement();
-  };
+const Counter: React.FC<CounterProps> = (
+    {
+        count,
+        canIncrement,
+        onIncrement,
+        onDecrement,
+    }
+) => {
+    const onMinusClick = () => {
+        if (count > 0) {
+            onDecrement();
+        }
+    };
 
-  const onPlusClick = () => {
-    if (canIncrement) onIncrement();
-  };
+    const onPlusClick = () => {
+        if (canIncrement) {
+            onIncrement();
+        }
+    };
 
-  return (
-    <div className={styles.wrapper}>
-      <span style={{width: 16,height: 16}} onClick={onMinusClick}>
-        <MinusIcons 
-          className={cs(styles.action, {
-            [styles.active]: count > 0,
-            [styles.inactive]: count <= 0,
-          })}
-        />
-      </span>
-      {count}
-      <AddIcon  onClick={onPlusClick}
-        style={{ color: "grey" }}
-        className={cs(styles.action, {
-          [styles.active]: canIncrement,
-          [styles.inactive]: !canIncrement,
-        })}
-      />
-    </div>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <MinusIcons
+                onClick={onMinusClick}
+                className={cs(styles.action, {
+                    [styles.inactive]: count <= 0,
+                })}
+            />
+
+            <span className={cs(styles.count, {
+                [styles.accented]: count > 0,
+            })}>
+                {count}
+            </span>
+
+            <AddIcon
+                onClick={onPlusClick}
+                className={cs(styles.action, {
+                    [styles.inactive]: !canIncrement,
+                })}
+            />
+        </div>
+    );
 };
 export default Counter;
