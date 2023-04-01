c38e18861fa892411eaa3a19e4c65a912bddaf43
JUtupe <petryszyn@o2.pl>
2023-04-01 14:52:07 +0200

    production build v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import { svgrComponent } from 'vite-plugin-svgr-component'
-import { config } from 'dotenv'
-
-config();
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-    process.env = {...process.env, ...loadEnv(mode, process.cwd(), 'RETRO_')};
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), 'RETRO_')
 
-    return defineConfig({
+    return {
         plugins: [svgrComponent(), react()],
         root: 'src',
         build: {
@@ -21,8 +18,8 @@ export default ({ mode }) => {
             port: 8080,
         },
         define: {
-            API_URL: process.env.RETRO_API_URL,
-            SOCKET_URL: process.env.RETRO_SOCKET_URL,
+            API_URL: env.RETRO_API_URL,
+            SOCKET_URL: env.RETRO_SOCKET_URL,
         }
-    })
-}
+    }
+})
